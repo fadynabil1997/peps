c0414b6efdf7a5045521de689475b1c4b07a7c55
music home <80252985+fadynabil1997@users.noreply.github.com>
2021-10-06 16:44:09 +0200

    Add files via upload

---
+++ pepsi.js
@@ -0,0 +1,49 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function bgcolor (color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
+
+
+// function bgcolor (color) {
+//     const user = document.querySelector('.user');
+//     user.style.background = color;
+// }
+
+function bgcolor1 (color1) {
+    const user = document.querySelector('.user1');
+    user.style.background = color1;
+}
+
+function bgcolor2 (color2) {
+    const user = document.querySelector('.user2');
+    user.style.background = color2;
+}
+
+function bgcolor3 (color3) {
+    const user = document.querySelector('.user3');
+    user.style.background = color3;
+}
+
+function bgcolor4 (color4) {
+    const user = document.querySelector('.user4');
+    user.style.background = color4;
+}
+
+
+// let user = document.getElementById('user');
+
+// imgSlider.onclick = function () {
+
+//     user.style.color = 'black';
+// }
+
+
+
+// imgSlider.onclick = function () {
+
+//     user.style.backgroundColor= color;
+// }
